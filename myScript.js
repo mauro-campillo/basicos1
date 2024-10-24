6dfe11667f00b1b80b57c104478a3e1ec498422b
mauro-campillo <22690386@tecvalles.mx>
2024-10-24 13:15:24 -0600

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    document.getElementById('demo3').style.fontSize = '35px';
+    document.getElementById("demo3").innerHTML = "Cambió con JS Externo a color ROJO y a 35px"; document.getElementById("demo3").style.color = "red";
+}
+
+function aumentar() {
+    document.getElementById('demo2').style.fontSize = '35px';
+    document.getElementById('demo2').style.color = 'red';
+}
